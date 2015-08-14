e2fa5f5fea585151c7ef330475a67c2d763e7462
jonboydell <jonboydell@hotmail.com>
2015-08-14 11:56:14 +0100

    added some older scripts

---
+++ create_pc_floppy.sh
@@ -0,0 +1,1 @@
+dd if=/dev/zero bs=1024 count=1440 > floppy.img
---
+++ install-binutils.sh
@@ -0,0 +1,7 @@
+cp ./binutils/addr2line $TARGET
+cp ./binutils/elfedit $TARGET
+cp ./binutils/libtool $TARGET
+cp ./binutils/objcopy $TARGET
+cp ./binutils/objdump $TARGET
+cp ./binutils/readelf $TARGET
+cp ./binutils/strings $TARGET
