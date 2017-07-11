106258f21a6eb96adbdad72af1c7f9455791eff0
jonboydell <jonboydell@hotmail.com>
2017-07-11 18:32:10 +0100

    better profile management

--- node-setup.sh
+++ node-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p ~/.nman/bin
 curl -O https://raw.githubusercontent.com/jonboydell/nman/master/node-manager.sh; source node-manager.sh;
 mv node-manager.sh ~/.nman/bin
 curl -o- -L https://yarnpkg.com/install.sh | bash
@@ -12,4 +13,5 @@ npm -g install nodemon
 
 echo export PATH=~/.nman/bin:\${PATH} >> ~/.profile
 echo export PATH=~/.yarn/bin:\${PATH} >> ~/.profile
-echo source ~/.nman/bin/node-manager.sh; nman-switch-active >> ~/.profile
+echo source ~/.nman/bin/node-manager.sh >> ~/.profile
+echo nman-switch-active >> ~/.profile
