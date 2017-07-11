7b7c35a042e9db525d11b521281ea7e1d528c246
jonboydell <jonboydell@hotmail.com>
2017-07-11 18:34:30 +0100

    Update node-setup.sh

--- node-setup.sh
+++ node-setup.sh
@@ -3,15 +3,13 @@
 mkdir -p ~/.nman/bin
 curl -O https://raw.githubusercontent.com/jonboydell/nman/master/node-manager.sh; source node-manager.sh;
 mv node-manager.sh ~/.nman/bin
-curl -o- -L https://yarnpkg.com/install.sh | bash
-
 export PATH=~/.nman/bin:${PATH}
-export PATH=~/.yarn/bin:${PATH}
-
 nman-switch v7.10.0
 npm -g install nodemon
-
 echo export PATH=~/.nman/bin:\${PATH} >> ~/.profile
-echo export PATH=~/.yarn/bin:\${PATH} >> ~/.profile
 echo source ~/.nman/bin/node-manager.sh >> ~/.profile
 echo nman-switch-active >> ~/.profile
+
+curl -o- -L https://yarnpkg.com/install.sh | bash
+export PATH=~/.yarn/bin:${PATH}
+echo export PATH=~/.yarn/bin:\${PATH} >> ~/.profile
